7e2e32228defe3312a42e68d57b79114658caa9b
Jason McNew <foonix@yahoo.com>
2022-03-19 17:28:40 -0400

    print caravan fight win/loss to combat log

---
+++ CaravanDataLoggerBehavior.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.MapEvents;
+using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.Core;
+
+namespace Caravanalyzer
+{
+    internal class CaravanDataLoggerBehavior : CampaignBehaviorBase
+    {
+
+        public override void RegisterEvents()
+        {
+            CampaignEvents.MobilePartyDestroyed.AddNonSerializedListener(this, OnPartyDestroyed);
+            CampaignEvents.MapEventEnded.AddNonSerializedListener(this, OnMapEventEnded);
+        }
+
+        private void OnMapEventEnded(MapEvent me)
+        {
+            if (!me.IsFieldBattle)
+            {
+                return;
+            }
+
+            foreach (var caravanParty in GetMobileCaravansFromSide(me.DefenderSide))
+            {
+                LogCaravanFight(caravanParty.MobileParty, me.Winner?.LeaderParty?.MobileParty, me.WinningSide == BattleSideEnum.Defender);
+            }
+        }
+
+        private IEnumerable<PartyBase> GetMobileCaravansFromSide(MapEventSide side)
+        {
+            foreach (var mep in side.Parties)
+            {
+                var mp = mep.Party?.MobileParty;
+                if (mp != null && mp.IsCaravan)
+                {
+                    yield return mep.Party;
+                }
+            }
+        }
+
+        private void LogCaravanFight(MobileParty caravan, MobileParty otherSideLeader, bool caravanWon)
+        {
+            string success;
+            if (otherSideLeader is null)
+            {
+                success = "drove away";
+            }
+            else
+            {
+                success = caravanWon ? "defeated" : "lost to";
+            }
+            InformationManager.DisplayMessage(new InformationMessage($"Caravan {caravan.Name} {success} {otherSideLeader.Name} at {caravan.GetPosition2D}"));
+        }
+
+        private void OnPartyDestroyed(MobileParty mp, PartyBase pb)
+        {
+            var isCaravan = mp.IsCaravan;
+            var pos = mp.Position2D;
+            //InformationManager.DisplayMessage(new InformationMessage($"party died is carvan {isCaravan} pos {pos}"));
+        }
+
+        public override void SyncData(IDataStore dataStore) { }
+    }
+}
---
+++ Caravanalyzer.cs
@@ -0,0 +1,31 @@
+﻿using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
+
+namespace Caravanalyzer
+{
+    public class Caravanalyzer : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            Module.CurrentModule.AddInitialStateOption(new InitialStateOption("Message",
+                new TextObject("Message", null),
+                9990,
+                () => { InformationManager.DisplayMessage(new InformationMessage("Hello World!")); },
+                () => { return (false, null); }
+            ));
+
+
+        }
+
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+        {
+            if (gameStarterObject is CampaignGameStarter gameStarter)
+            {
+                gameStarter.AddBehavior(new CaravanDataLoggerBehavior());
+                //gameStarter.AddModel(new ModBanditMilitiaKillModel());
+            }
+        }
+    }
+}
